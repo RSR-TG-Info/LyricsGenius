5f3c58ee5a97a4f2ac5e47ea434be9af35787fe6
RSR Official <80620448+RSR-TG-Info@users.noreply.github.com>
2022-08-18 23:25:43 +0530

    Update genius.py

--- lyricsgenius/genius.py
+++ lyricsgenius/genius.py
@@ -142,10 +142,10 @@ class Genius(API, PublicAPI):
                       "Song URL: https://genius.com/{}".format(path))
             return None
         else:
-            #rem = div.find("div", class_=re.compile("Lyrics__Footer"))
-            #if rem:
-                #rem.replace_with("")
-            header = div.find("h2", class_=re.compile("Lyrics__Title"))
+            rem = div.find("div", class_=re.compile("Lyrics__Footer"))
+            if rem:
+                rem.replace_with("")
+            header = div.find("h2", class_=re.compile("TextLabel"))
             if header:
                 header.replace_with("")
         lyrics = div.get_text()
